aab38ce9c986611bec5b84d9aa88af5f4a202fdc
Thanh Tai Nguyen <tainguyen.j.16@gmail.com>
2026-09-19 16:36:31 +1000

    Update testDb.js

--- tests/helpers/testDb.js
+++ tests/helpers/testDb.js
@@ -2,12 +2,10 @@
 
 const database = require('../../src/config/database');
 
-/** Opens a clean in-memory database for a test file. */
 function setupDb() {
   database.connect();
 }
 
-/** Empties every table between tests so cases cannot leak state into each other. */
 function resetDb() {
   const db = database.getDb();
   db.exec('DELETE FROM tasks; DELETE FROM projects; DELETE FROM users;');
